fcabc21fceab8ce671be36441ccd5f894f4b1781
obcat <obcat@icloud.com>
2020-07-16 17:21:18 +0900

    Initial Commit

---
+++ .zshrc
@@ -0,0 +1,2 @@
+# コマンドプロンプトのカスタマイズ
+export PROMPT="%F{magenta}%n%f@%F{cyan}%m%f%F{green} [%~]%f %# "
---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for f in .??*
+do
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".DS_Store" ]] && continue
+
+    ln -s $(pwd)/${f} ${HOME}/${f}
+done
