b71563e4b8e401c9c34565f2e2eaeab51521f58d
obcat <obcat@icloud.com>
2020-08-20 21:33:33 +0900

    remove unused code

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 for f in .??*
 do
     [[ ${f} == '.git' ]]       && continue
-    [[ ${f} == '.gitignore' ]] && continue
     [[ ${f} == '.DS_Store'  ]] && continue
 
     ln -sfnv ~/dotfiles/${f} ${HOME}/${f}
