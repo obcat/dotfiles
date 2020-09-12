4d9c1028ac7fc8fcde884147b9a4d16ea4a84f17
obcat <obcat@icloud.com>
2020-09-12 15:59:31 +0900

    Show error msg if dotfiles don't exist

--- install.sh
+++ install.sh
@@ -8,6 +8,11 @@ for fpath in dotfiles/home/.??*
 do
   fname=$(basename "$fpath")
 
+  if [ "$fname" = '.??*' ]; then
+    echo 'error: no dotfiles in ~/dotfiles/home/' 1>&2
+    break
+  fi
+
   if [ -e "$fname" ] && [ ! -h "$fname" ]; then
     echo -n 'backup) '
     mv -vf "$fname" "${fname}.bak"
