c702f172c0b5cdfe7bb94cbea821de7ff5b0f8be
obcat <obcat@icloud.com>
2020-09-13 14:08:28 +0900

    Backup existing dotfile recursively

--- install.sh
+++ install.sh
@@ -4,22 +4,32 @@
 
 cd ~
 
-virtualhome='dotfiles/home'
+readonly VIRTUAL_HOME='dotfiles/home'
 
-for fpath in "${virtualhome}"/.??*
+rename_or_remove_if_exist() {
+  if [[ -e "$1" ]]; then
+    if [[ -h "$1" ]]; then
+      rm "$1"
+    else
+      rename_or_remove_if_exist "${1}.bak"
+
+      echo -n 'backup) '
+      mv -v "$1" "${1}.bak"
+    fi
+  fi
+}
+
+for fpath in "${VIRTUAL_HOME}"/.??*
 do
   fname=$(basename "${fpath}")
 
-  if [ "${fname}" = '.??*' ]; then
-    echo "error: no dotfiles in ~/${virtualhome}/" 1>&2
-    break
+  if [[ "${fname}" == '.??*' ]]; then
+    echo "error: no dotfiles in ~/${VIRTUAL_HOME}" 1>&2
+    exit 1
   fi
 
-  if [ -e "${fname}" ] && [ ! -h "${fname}" ]; then
-    echo -n 'backup) '
-    mv -vf "${fname}" "${fname}.bak"
-  fi
+  rename_or_remove_if_exist "${fname}"
 
   echo -n 'link) '
-  ln -svnf "${fpath}" "${fname}"
+  ln -sv "${fpath}" "${fname}"
 done
